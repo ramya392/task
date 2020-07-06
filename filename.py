215a377cdaaf3a51d110b3ab15cc29a6751efdb9
ramya392 <67824436+ramya392@users.noreply.github.com>
2020-07-06 17:02:07 +0530

    radius and filename
    prints radius and extension of filename

---
+++ filename.py
@@ -0,0 +1,3 @@
+filename=input("Input the filename :")
+filename_extns=filename.split(".")
+print("The extension of the file is :" +repr(filename_extns[-1]))
---
+++ radius.py
@@ -0,0 +1,5 @@
+from math import pi
+a=float(input("The radius of the circle :"))
+pi=22/7
+print("The area of the circle with radius " +str(a)+ " is " +str(pi*a**2))
+                                                
