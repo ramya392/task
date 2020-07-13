82a65a3a1ad0708fb226a8cbb5442ded7796e7e8
ramya392 <67824436+ramya392@users.noreply.github.com>
2020-07-13 18:21:35 +0530

    radius and filename
    prints radius and extension if file

--- radius.py
+++ radius.py
@@ -1,5 +1,4 @@
 from math import pi
 a=float(input("The radius of the circle :"))
-pi=22/7
 print("The area of the circle with radius " +str(a)+ " is " +str(pi*a**2))
                                                 
